baeaba4be1cd7be12c1bb13bf7a18ab796dc36c5
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2021-06-06 00:03:01 +0800

    saving student to database

--- controller/student.js
+++ controller/student.js
@@ -1,13 +1,40 @@
+const Student = require('../model/Student');
 
 
+//get home page for student
 exports.getStudent = (req, res) =>{
     res.render('student/student', {
         pageTitle: 'Student'
     });
 }
 
+//get register page
 exports.getRegister = (req, res) =>{
     res.render('student/register', {
         pageTitle: 'Registration'
     });
+}
+
+
+//post register student
+exports.postRegisterStudent = (req, res) =>{
+    const {fname,lname,cnumber,email,password} = req.body;
+
+    Student.create({
+        fname: fname,
+        lname: lname,
+        cnumber: cnumber,
+        email: email,
+        password: password,
+    })
+
+    .then((student) =>{
+        console.log('Student created', student);
+        res.redirect('/');
+    })
+
+    .catch((err) =>{
+        console.log(err)
+    })
+
 }
--- routes/student.js
+++ routes/student.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const {getStudent, getRegister} = require('../controller/student');
+const {getStudent, getRegister, postRegisterStudent} = require('../controller/student');
 
 //get student  home page
 router.get('/', getStudent);
 
 //register page
 router.get('/register', getRegister);
+
+//post register student account
+router.post('/register', postRegisterStudent);
+
 
 
 module.exports = router;
