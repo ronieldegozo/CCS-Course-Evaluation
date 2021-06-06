5bef38fb754562db808eb4234533a4f988ea42e0
Mc Roniel De Gozo <ronieldegozo@gmail.com>
2021-06-06 16:38:50 +0800

    email student to admin

--- routes/student.js
+++ routes/student.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const {getStudent, getRegister, postRegisterStudent} = require('../controller/student');
+const { postEmail} = require('../controller/email');
+
 
 //get student  home page
 router.get('/', getStudent);
@@ -10,6 +12,9 @@ router.get('/register', getRegister);
 
 //post register student account
 router.post('/register', postRegisterStudent);
+
+//email student inquiry
+router.post('/email', postEmail);
 
 
 
